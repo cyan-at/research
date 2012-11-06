e875b503a6351616167fedc66d7eb847831613bf
Charlie Yan <charlieyan08@gmail.com>
2012-11-06 07:07:32 -0500

    changes

--- regiongrowing/code/matlab/tune.m
+++ regiongrowing/code/matlab/tune.m
@@ -5,9 +5,9 @@ rs = [10, 20, 30, 40, 50];
 ms = [50, 100, 150, 200, 250];
 as = [15000];
 infiles = {...
-	'pcd0000000024.pcd'
+	'/home/charlie/Desktop/research/regiongrowing/code/pclcode/data/source/pcd0000000024.pcd'
 };
-outdir = './data/';
+outdir = '/home/charlie/Desktop/research/regiongrowing/code/pclcode/data/';
 
 count = 0;
 for k in ks
@@ -16,17 +16,17 @@ for k in ks
 			for a in as
 				for infile in infiles
 					% search hyperparameters for multiple files
-					resultsFile = sprintf('./data/%s.txt', num2str(count));
+					resultsFile = sprintf('/home/charlie/Desktop/research/regiongrowing/code/pclcode/data/%s.txt', num2str(count));
 					cmd = sprintf('/home/charlie/Desktop/research/regiongrowing/code/pclcode/segment.out -i %s -o %s -k %s -r %s -m %s -a %s > %s', ...
 						cell2mat(infile), outdir, num2str(k), num2str(r), num2str(m), num2str(a), resultsFile);
 					system(cmd);
 
 					% clean up
-					system('rm ./data/*.pcd');
+					system('rm /home/charlie/Desktop/research/regiongrowing/code/pclcode/data/*.pcd');
 					count = count + 1;
 				end
 			end
 		end
 	end
 end
-
+% at this point we should have a bunch of txt files
