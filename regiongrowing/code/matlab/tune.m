d6f81eb4a4448f1ac56677cba85d2e30403bc933
Charlie Yan <charlieyan08@gmail.com>
2012-11-06 07:02:52 -0500

    changes

--- regiongrowing/code/matlab/tune.m
+++ regiongrowing/code/matlab/tune.m
@@ -5,26 +5,28 @@ rs = [10, 20, 30, 40, 50];
 ms = [50, 100, 150, 200, 250];
 as = [15000];
 infiles = {...
-	'pcd0000000024.pcd',...
-	'pcd0000000024.pcd',...
-	'pcd0000000024.pcd',...
-	'pcd0000000024.pcd'...
+	'pcd0000000024.pcd'
 };
+outdir = './data/';
+
+count = 0;
 for k in ks
 	for r in rs
 		for m in ms
 			for a in as
-				% search hyperparameters for multiple files
-				infile = 'data/pcd0000000024.pcd';
-				outdir = './data/';
-				k = num2str(30);
-				r = num2str(20);
-				m = num2str(100);
-				a = num2str(15000);
-				resultFile = sprintf()
-				cmd = sprintf('/home/charlie/Desktop/research/regiongrowing/code/pclcode/segment.out -i %s -o %s -k %s -r %s -m %s -a %s > ', infile, outdir, k, r, m, a);
+				for infile in infiles
+					% search hyperparameters for multiple files
+					resultsFile = sprintf('./data/%s.txt', num2str(count));
+					cmd = sprintf('/home/charlie/Desktop/research/regiongrowing/code/pclcode/segment.out -i %s -o %s -k %s -r %s -m %s -a %s > %s', ...
+						cell2mat(infile), outdir, num2str(k), num2str(r), num2str(m), num2str(a), resultsFile);
+					system(cmd);
 
+					% clean up
+					system('rm ./data/*.pcd');
+					count = count + 1;
+				end
 			end
 		end
 	end
 end
+
