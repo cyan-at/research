7192f8f51abfa0e16b70e5c55142a6866b88d589
Charlie Yan <charlieyan08@gmail.com>
2012-11-12 07:08:26 -0500

    changes

---
+++ code/classification/classifyClusters.m
@@ -0,0 +1,9 @@
+function classifyClusters(clusterLocation, trainedModelLocation)
+% performs binary classification on segmentation results
+% path is the location of the cluster pcd files
+% first, convert them all to mat files
+% then compute spin images on them
+% then do pooling
+% then apply them to trained models
+
+end
