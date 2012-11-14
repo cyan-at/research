f911d342fe2093e1db6420db8a5e9e165f66d05d
Charlie Yan <charlieyan08@gmail.com>
2012-11-14 17:49:47 -0500

    changes

--- code/classification/run.m
+++ code/classification/run.m
@@ -32,7 +32,7 @@ for i = 1:length(noCarFolders)
     classifyClusters(clusterLoc, encoder, model, parameters);
 end
 %% this part of the code will call merge on all of the labelled clusters
-merge_all_path = '/home/charlie/Desktop/research/code/utilities/c/mergePCD/test/mergeAll -i %s -o %s';
+merge_all_path = '/home/charlie/Desktop/research/code/utilities/c/mergePCD/test/mergeAll %s -o %s';
 for i = 1:length(carFolders)
     carpcds = strcat(cell2mat(carFolders(i)), '/don/classified/pcd/car_*.pcd');
     notpcds = strcat(cell2mat(carFolders(i)), '/don/classified/pcd/not_*.pcd');
