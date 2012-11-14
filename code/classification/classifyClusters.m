942996bd2f95b5da08c30974c0b74a5f8bc175ce
Charlie Yan <charlieyan08@gmail.com>
2012-11-14 17:45:42 -0500

    changes

--- code/classification/classifyClusters.m
+++ code/classification/classifyClusters.m
@@ -1,33 +1,31 @@
-function classifyClusters(clusterLocation, encoder, model)
+function classifyClusters(clusterLocation, encoder, model, parameters)
 addpath(genpath('/home/charlie/Desktop/research/code/library/'));
 % performs binary classification on segmentation results
 % path is the location of the cluster pcd files
 % first, convert them all to mat files
-matDir = fullfile(clusterLocation,'/../mat/');
-disp(matDir);
+matDir = fullfile(clusterLocation,'/../classified/mat/');
+pcdDir = fullfile(clusterLocation,'/../classified/pcd/');
+% disp(matDir);
 if (~exist(matDir,'dir'))
     mkdir(matDir);
+end
+if (~exist(pcdDir,'dir'))
+    mkdir(pcdDir);
 end
 % parameters
 radius = 1;
 imgW = 16;
 minN = 10;
-
-% load model, encoder
-load model.mat
-load svm.mat
-
 d = dir(clusterLocation);
 nameFolds = {d.name}';
 nameFolds(ismember(nameFolds,{'.','..'})) = [];
+numCars = 0;
+numNots = 0;
 for i = 1:length(nameFolds)
     pcdFile = fullfile(clusterLocation,cell2mat(nameFolds(i)));
     % disp(pcdFile);
     pc = pcd2mat(pcdFile);
     [~,y,z] = fileparts(pcdFile);
-    matFileName = fullfile(matDir,strcat(y,'.mat'));
-    disp(matFileName);
-    save(matFileName,'pc');
     % then compute spin images on them
     feaArr = compSpinImages(pc', radius, imgW, minN);
     feaArr = reshape(feaArr,imgW*imgW,size(pc,2));
@@ -38,10 +36,31 @@ for i = 1:length(nameFolds)
     feat.y = [1:size(feaArr,2)];
     % save(matFileName,'feat');
     % then do pooling
+    disp('computing pool');
     pool = pooling(feat, encoder, parameters);
-    % then apply them to trained models
-    
+    obj = struct();
+    obj.pool = pool;
+    obj.pc = pc;
     % save the pool
-    save(matFileName,'pool');
+    % save(matFileName,'pool');
+    % then apply them to trained models
+    [label, ~, ~] = predict(double(1),sparse(pool),model,[],'col');
+    % disp(label);
+    if (label == 1)
+        numCars = numCars + 1;
+        name = strcat('car_',num2str(numCars));
+    else
+        numNots = numNots + 1;
+        name = strcat('not_',num2str(numNots));
+    end 
+    % save the pool and point cloud under this name
+    matFileName = fullfile(matDir,strcat(name,'.mat'));
+    disp(matFileName);
+    save(matFileName,'obj');
+    % save the point cloud under that name
+    newpcdFileName = fullfile(pcdDir,strcat(name,'.pcd'));
+    cpCmd = sprintf('cp %s %s',pcdFile,newpcdFileName);
+    disp(cpCmd);
+    system(cpCmd);
 end
 end
--- code/classification/run.m
+++ code/classification/run.m
@@ -3,22 +3,57 @@ clc;
 targetDir = '/home/charlie/Desktop/research/data/segmentation/';
 targetHasCar = strcat(targetDir,'hascar/');
 targetNoCar = strcat(targetDir, 'nocar/');
-modelLoc = '/home/charlie/Desktop/research/data/';
+% load model, encoder
+load model.mat
+load svm.mat
+load encoder.mat
+% add path to predict
+addpath(genpath('/home/charlie/Desktop/research/code/classification/svmlinear/'));
+parameters = struct();
+parameters.pyramid = [1 2];
 d = dir(targetHasCar);
 isub = [d(:).isdir];
-nameFolds = {d(isub).name}';
-nameFolds(ismember(nameFolds,{'.','..'})) = [];
-for i = 1:length(nameFolds)
-    clusterLoc = strcat(cell2mat(nameFolds(i)), '/don/cluster/');
+carFolders = {d(isub).name}';
+carFolders(ismember(carFolders,{'.','..'})) = [];
+for i = 1:length(carFolders)
+    clusterLoc = strcat(cell2mat(carFolders(i)), '/don/cluster/');
+    clusterLoc = fullfile(targetHasCar,clusterLoc);
     disp(clusterLoc);
-    classifyClusters(clusterLoc, modelLoc);
+    classifyClusters(clusterLoc, encoder, model, parameters);
 end
 d = dir(targetNoCar);
 isub = [d(:).isdir];
-nameFolds = {d(isub).name}';
-nameFolds(ismember(nameFolds,{'.','..'})) = [];
-for i = 1:length(nameFolds)
-    clusterLoc = strcat(cell2mat(nameFolds(i)), '/don/cluster/');
+noCarFolders = {d(isub).name}';
+noCarFolders(ismember(noCarFolders,{'.','..'})) = [];
+for i = 1:length(noCarFolders)
+    clusterLoc = strcat(cell2mat(noCarFolders(i)), '/don/cluster/');
+    clusterLoc = fullfile(targetNoCar,clusterLoc);
     disp(clusterLoc);
-    classifyClusters(clusterLoc, modelLoc);
+    classifyClusters(clusterLoc, encoder, model, parameters);
 end
+%% this part of the code will call merge on all of the labelled clusters
+merge_all_path = '/home/charlie/Desktop/research/code/utilities/c/mergePCD/test/mergeAll -i %s -o %s';
+for i = 1:length(carFolders)
+    carpcds = strcat(cell2mat(carFolders(i)), '/don/classified/pcd/car_*.pcd');
+    notpcds = strcat(cell2mat(carFolders(i)), '/don/classified/pcd/not_*.pcd');
+    carInfiles = fullfile(targetHasCar,carpcds);
+    notInfiles = fullfile(targetHasCar, notpcds);
+    carNames = evalc(sprintf('ls %s', carInfiles));
+    notNames = evalc(sprintf('ls %s', notInfiles));
+    disp(carNames);
+    disp(notNames);
+end
+
+for i = 1:length(noCarFolders)
+    carpcds = strcat(cell2mat(noCarFolders(i)), '/don/classified/pcd/car_*.pcd');
+    notpcds = strcat(cell2mat(noCarFolders(i)), '/don/classified/pcd/not_*.pcd');
+    carInfiles = fullfile(targetNoCar,carpcds);
+    notInfiles = fullfile(targetNoCar, notpcds);
+    mergeCarCmd = sprintf(merge_all_path, carpcds, 
+    mergeNotCmd = 
+    disp(clusterLoc);
+    classifyClusters(clusterLoc, encoder, model, parameters);
+end
+
+
+
