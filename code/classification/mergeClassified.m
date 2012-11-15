d2c2d3c95a9fb65815fb44c5c681baf33aac3ae2
Charlie Yan <charlieyan08@gmail.com>
2012-11-14 22:05:50 -0500

    changes

--- code/classification/mergeClassified.m
+++ code/classification/mergeClassified.m
@@ -7,29 +7,33 @@ d = dir(targetHasCar);
 isub = [d(:).isdir];
 carFolders = {d(isub).name}';
 carFolders(ismember(carFolders,{'.','..'})) = [];
-merge_all_path = '/home/charlie/Desktop/research/code/utilities/c/mergePCD/test/mergeAll %s -o %s';
+mergePrefix = '/home/charlie/Desktop/research/code/utilities/c/mergePCD/test/mergeAll';
 for i = 1:length(carFolders)
     carpcds = strcat(cell2mat(carFolders(i)), '/don/classified/pcd/car_*.pcd');
+    carpcdDir = fullfile(targetHasCar,strcat(cell2mat(carFolders(i)), '/don/classified/pcd/'));
     notpcds = strcat(cell2mat(carFolders(i)), '/don/classified/pcd/not_*.pcd');
+    notpcdDir = fullfile(targetHasCar,strcat(cell2mat(carFolders(i)), '/don/classified/pcd/'));
     carInfiles = fullfile(targetHasCar,carpcds);
     notInfiles = fullfile(targetHasCar, notpcds);
-    carNames = evalc(sprintf('ls %s', carInfiles));
-    notNames = evalc(sprintf('ls %s', notInfiles));
-    disp(carNames);
-    disp(notNames);
-end
-
-d = dir(targetNoCar);
-isub = [d(:).isdir];
-noCarFolders = {d(isub).name}';
-noCarFolders(ismember(noCarFolders,{'.','..'})) = [];
-for i = 1:length(noCarFolders)
-    carpcds = strcat(cell2mat(noCarFolders(i)), '/don/classified/pcd/car_*.pcd');
-    notpcds = strcat(cell2mat(noCarFolders(i)), '/don/classified/pcd/not_*.pcd');
-    carInfiles = fullfile(targetNoCar,carpcds);
-    notInfiles = fullfile(targetNoCar, notpcds);
-    carNames = evalc(sprintf('ls %s', carInfiles));
-    notNames = evalc(sprintf('ls %s', notInfiles));
-    disp(carNames);
-    disp(notNames);
+    carNames = dir(carInfiles);
+    notNames = dir(notInfiles);
+    arguments = '';
+    for i = 1:length(carNames)
+        z = fullfile(carpcdDir,carNames(i).name);
+        arguments = sprintf('%s %s', arguments, z);
+    end
+    carspcdLoc = fullfile(carpcdDir, 'cars.pcd');
+    arguments = sprintf('%s -o %s',arguments, carspcdLoc);
+    mergeCmd = sprintf('%s %s', mergePrefix, arguments);
+    system(mergeCmd);
+    arguments = '';
+    for i = 1:length(notNames)
+        z = fullfile(notpcdDir, notNames(i).name);
+        arguments = sprintf('%s %s', arguments, z);
+    end
+    notpcdLoc = fullfile(notpcdDir, 'not.pcd');
+    arguments = sprintf('%s -o %s',arguments, notpcdLoc);
+    mergeCmd = sprintf('%s %s', mergePrefix, arguments);
+    system(mergeCmd);
+    x = 1;
 end
