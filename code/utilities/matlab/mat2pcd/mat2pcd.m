73ae497781220edef36ce647a07568b4b030f504
Charlie Yan <charlieyan08@gmail.com>
2012-11-15 09:22:56 -0500

    mat2pcd

--- code/utilities/matlab/mat2pcd/mat2pcd.m
+++ code/utilities/matlab/mat2pcd/mat2pcd.m
@@ -1,7 +1,18 @@
 function mat2pcd(pointcloud, saveName)
 %assumes that pointcloud is a N x 3 matrix of values
 %converts the file to PCD format somewhat and saves it at saveName
-fileID = fopen(saveName,'w');
-fprintf(fid, 'Hello!\n');
-fclose(fileID);
+fid = fopen(saveName,'w');
+fprintf(fid, '# .PCD v0.7 - Point Cloud Data file format\nVERSION 0.7\nFIELDS x y z\n');
+fprintf(fid,'SIZE 4 4 4\n');
+fprintf(fid,'TYPE F F F\n');
+fprintf(fid,'COUNT 1 1 1\n');
+pointCountStr = sprintf('POINTS %s\n', num2str(size(pointcloud,1)));
+fprintf(fid, pointCountStr);
+fprintf(fid,'VIEWPOINT 0 0 0 1 0 0 0\n');
+fprintf(fid,'DATA ascii\n');
+% begin points
+for i = 1:size(pointcloud,1)
+    fprintf(fid,sprintf('%s\n',num2str(pointcloud(i,:))));
+end
+fclose(fid);
 end
