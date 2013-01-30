1fcea48b907cd8d17de298c4b43c0885a86d545d
Charlie Yan <charlieyan08@gmail.com>
2013-01-30 13:15:32 -0500

    start working on code

--- code/3dproject/library/functions/catalogue.m
+++ code/3dproject/library/functions/catalogue.m
@@ -1,13 +1,13 @@
-function [ catalog ] = catalogue( featurePath, parameters )
+function [ catalog ] = catalogue( path, extension )
 %takes a directory, and the parameters about that directory of features,
 %and creates a 'catalogue' array of paths
     catalog = {};
-    fpath = dir(featurePath);
+    fpath = dir(path);
     for i = 1:length(fpath)
         matName = fpath(i).name;
         first_char = matName(1);
         if (~strcmp(matName,'featureMatrix.mat')) && (~strcmp(first_char,'.')) && (~strcmp(matName, 'parameters.txt'))
-            catalog{end+1} = fullfile(featurePath, matName);
+            catalog{end+1} = fullfile(path, matName);
         end
     end
 end
