d0a3e2ddeac1bf03623589425f026d9e062da3dc
Charlie Yan <charlieyan08@gmail.com>
2013-01-31 00:02:31 -0500

    housecleaning

--- code/3dproject/segmentation/ford/run3d.m
+++ code/3dproject/segmentation/ford/run3d.m
@@ -6,22 +6,57 @@ scanFolderRoot = '/mnt/neocortex/scratch/jumpbot/data/3dproject/FordScans/';
 if (~exist(scanFolderRoot,'dir')); mkdir(scanFolderRoot); end;
 scansDir = strcat(dataRoot,'SCANS');
 scans = catalogue(scansDir,'mat');
+prefix = '/home/charlie/Desktop/research/code/segmentation/c/distanceNormals/build/don %s .2 2 0.25 0.75 %s';
+start = false;
+if (start)
 for i = 1:length(scans)
 	scanFile = cell2mat(scans(i));
 	%mkdir a directory with the name being this scan's name
 	[~,y,~] = fileparts(scanFile);
-	scanDir = strcat(scanFolderRoot,y);
+	scanDir = strcat(scanFolderRoot,y,'/');
 	if (~exist(scanDir,'dir')); mkdir(scanDir); end;
 	%load up the point cloud
 	targetMatName = strcat(scanDir,y,'.mat');
 	cpCmd = sprintf('cp %s %s', scanFile, targetMatName);
 	system(cpCmd);
 	%grab the ppm from the obj and copy it over to the scan directory
-	targetImgName = strcat(scanDir, 'image.ppm');
-	cpCmd = sprintf('cp %s %s', obj.img, targetImgName);
-	disp(targetImgName);
-	system(cpCmd);	
-	%make a pcd file
-	%apply DON pipeline on the pointcloud
-	
+    clear SCAN; load(targetMatName); index = SCAN.image_index;
+    cam0 = strcat(dataRoot,'IMAGES/Cam0/image0',num2str(index),'.ppm');
+    cam1 = strcat(dataRoot,'IMAGES/Cam1/image0',num2str(index),'.ppm');
+    cam2 = strcat(dataRoot, 'IMAGES/Cam2/image0',num2str(index),'.ppm');
+    cam3 = strcat(dataRoot, 'IMAGES/Cam3/image0',num2str(index),'.ppm');
+    cam4 = strcat(dataRoot, 'IMAGES/Cam4/image0',num2str(index),'.ppm');
+    targetImgName0 = strcat(scanDir, 'image0.ppm');
+	cpCmd = sprintf('cp %s %s', cam0, targetImgName0);
+    system(cpCmd);	
+    targetImgName1 = strcat(scanDir, 'image1.ppm');
+	cpCmd = sprintf('cp %s %s', cam1, targetImgName1);    
+    system(cpCmd);
+    targetImgName2 = strcat(scanDir, 'image2.ppm');
+	cpCmd = sprintf('cp %s %s', cam2, targetImgName2);
+    system(cpCmd);	
+    targetImgName3 = strcat(scanDir, 'image3.ppm');
+	cpCmd = sprintf('cp %s %s', cam3, targetImgName3);
+    system(cpCmd);
+    targetImgName4 = strcat(scanDir, 'image4.ppm');
+	cpCmd = sprintf('cp %s %s', cam4, targetImgName4);
+    system(cpCmd);
+	disp(y);
+end
+end
+start = true;
+if (start)
+%%in this section we will extract pcl from every scan file and run DON
+%%clustering
+for i = 1:length(scans)
+	scanFile = cell2mat(scans(i));
+	%mkdir a directory with the name being this scan's name
+	[~,y,~] = fileparts(scanFile);
+	scanDir = strcat(scanFolderRoot,y,'/');
+	%grab the ppm from the obj and copy it over to the scan directory
+    clear SCAN; load(targetMatName);
+    pc = SCAN.XYZ;
+    mat2pcd(
+	disp(y);
+end
 end
--- code/3dproject/utilities/pcl/cmakeEigen.m
+++
@@ -1,9 +0,0 @@
-cmakebinLocation = '/mnt/neocortex/scratch/jumpbot/dependencies/cmakebuild/bin/cmake';
-flag = '-D CMAKE_INSTALL_PREFIX=/mnt/neocortex/scratch/jumpbot/dependencies/eigenbuild/';
-currentDir = pwd;
-eigenDir = '/mnt/neocortex/scratch/jumpbot/dependencies/eigen/build_dir/';
-cd(eigenDir);
-cmd = sprintf('%s %s ..', cmakebinLocation, flag);
-disp(cmd);
-system(cmd);
-cd(currentDir);
