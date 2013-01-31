4e451c441c66976e87fcdf04bc9335a4052decbf
Charlie Yan <charlieyan08@gmail.com>
2013-01-30 22:59:26 -0500

    run3d

--- code/3dproject/segmentation/ford/run3d.m
+++ code/3dproject/segmentation/ford/run3d.m
@@ -7,17 +7,21 @@ if (~exist(scanFolderRoot,'dir')); mkdir(scanFolderRoot); end;
 scansDir = strcat(dataRoot,'SCANS');
 scans = catalogue(scansDir,'mat');
 for i = 1:length(scans)
-scanFile = cell2mat(scans(i));
-%mkdir a directory with the name being this scan's name
-[~,y,~] = fileparts(scanFile);
-disp(y);
-scanDir = strcat(scanFolderRoot,y);
-if (~exist(scanDir,'dir')); mkdir(scanDir); end;
-%load up the point cloud
-pc = 
-%make a pcd file
-
-%apply DON pipeline on the pointcloud
-
-%
+	scanFile = cell2mat(scans(i));
+	%mkdir a directory with the name being this scan's name
+	[~,y,~] = fileparts(scanFile);
+	scanDir = strcat(scanFolderRoot,y);
+	if (~exist(scanDir,'dir')); mkdir(scanDir); end;
+	%load up the point cloud
+	targetMatName = strcat(scanDir,y,'.mat');
+	cpCmd = sprintf('cp %s %s', scanFile, targetMatName);
+	system(cpCmd);
+	%grab the ppm from the obj and copy it over to the scan directory
+	targetImgName = strcat(scanDir, 'image.ppm');
+	cpCmd = sprintf('cp %s %s', obj.img, targetImgName);
+	disp(targetImgName);
+	system(cpCmd);	
+	%make a pcd file
+	%apply DON pipeline on the pointcloud
+	
 end
