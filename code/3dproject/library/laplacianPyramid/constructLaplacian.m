45a8c377dc81d6d43977f31d862bdaf8e62a663c
Charlie Yan <charlieyan08@gmail.com>
2013-02-11 17:57:12 -0500

    changes

--- code/3dproject/library/laplacianPyramid/constructLaplacian.m
+++
@@ -1,8 +0,0 @@
-function [ laplacianStruct ] = constructLaplacian( imageName, depth )
-    %Take the image and some arguments and construct the laplacian pyramid for
-    %the image
-    im = imread(imageName);
-    im = double(im)/256;
-    
-end
-
